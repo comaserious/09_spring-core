c9f6a3e70fef3c1f8dfb3f7190ba18ce31d4f72c
leehojun <sciboviator@gmail.com>
2024-04-09 16:47:21 +0900

    추가내용

--- 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfiguration.java
+++ 01_lecture-source/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/ContextConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan(basePackages = "com.ohgiraffers.section01.aop")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ContextConfiguration {
-
+    // proxy를 지정해주어야지 @Aspect 를 ApplicationContext 에 막는 형태로 적절히 감싸준다
 
 
 }
